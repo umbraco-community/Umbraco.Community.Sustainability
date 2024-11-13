2d222d39dfed3ff41b040f82866cea73dfbb19c2
Rick Butterfield <rick.butterfield@gmail.com>
2024-11-13 13:39:36 +0000

    Remove unused properties

--- src/Umbraco.Community.Sustainability/Models/SustainabilityData.cs
+++ src/Umbraco.Community.Sustainability/Models/SustainabilityData.cs
@@ -12,55 +12,12 @@ namespace Umbraco.Community.Sustainability.Models
     {
         public double? co2 { get; set; }
         public bool green { get; set; }
-        public Variables? variables { get; set; }
-    }
-
-    public record Variables
-    {
-        public string? description { get; set; }
-        public int? bytes { get; set; }
-        public GridIntensity? gridIntensity { get; set; }
-        public double? dataReloadRatio { get; set; }
-        public double? firstVisitPercentage { get; set; }
-        public double? returnVisitPercentage { get; set; }
-    }
-
-    public record GridIntensity
-    {
-        public string? description { get; set; }
-        public double? network { get; set; }
-        public double? dataCenter { get; set; }
-        public double? production { get; set; }
-        public double? device { get; set; }
     }
 
     public record Resource
     {
         public string? name { get; set; }
-        public string? entryType { get; set; }
-        public double? startTime { get; set; }
-        public double? duration { get; set; }
         public string? initiatorType { get; set; }
-        public string? deliveryType { get; set; }
-        public string? nextHopProtocol { get; set; }
-        public string? renderBlockingStatus { get; set; }
-        public double? workerStart { get; set; }
-        public double? redirectStart { get; set; }
-        public double? redirectEnd { get; set; }
-        public double? fetchStart { get; set; }
-        public double? domainLookupStart { get; set; }
-        public double? domainLookupEnd { get; set; }
-        public double? connectStart { get; set; }
-        public double? secureConnectionStart { get; set; }
-        public double? connectEnd { get; set; }
-        public double? requestStart { get; set; }
-        public double? responseStart { get; set; }
-        public double? firstInterimResponseStart { get; set; }
-        public double? responseEnd { get; set; }
         public int? transferSize { get; set; }
-        public int? encodedBodySize { get; set; }
-        public int? decodedBodySize { get; set; }
-        public int? responseStatus { get; set; }
-        public object[] serverTiming { get; set; }
     }
 }
