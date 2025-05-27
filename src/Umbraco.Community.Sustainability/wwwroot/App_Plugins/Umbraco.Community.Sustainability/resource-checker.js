20e4853bb53c153cf25c0253299dbbb4dd2d7073
Rick Butterfield <rick.butterfield@gmail.com>
2025-05-27 13:50:54 +0100

    Add rebuilt script

--- src/Umbraco.Community.Sustainability/wwwroot/App_Plugins/Umbraco.Community.Sustainability/resource-checker.js
+++ src/Umbraco.Community.Sustainability/wwwroot/App_Plugins/Umbraco.Community.Sustainability/resource-checker.js
@@ -1,6 +1,6 @@
 import { co2, hosting } from 'https://cdn.skypack.dev/@tgwf/co2@0.15';
 
-async function reportEmissions() {
+export async function reportEmissions() {
   console.log("reportEmissions called");
 
   await scrollPage();
@@ -35,8 +35,6 @@ async function getEmissionsData() {
   const co2Emission = new co2({ model: "swd" });
   const emissions = co2Emission.perVisitTrace(bytesSent, hostCheck);
 
-  debugger;
-
   return {
     pageWeight: bytesSent,
     carbonRating: calculateGrade(emissions.co2),
@@ -64,5 +62,3 @@ function calculateGrade(score) {
   if (score < 0.846) return 'E';
   return 'F';
 }
-
-await reportEmissions();
